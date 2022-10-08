e14c7a004f44a33b23581bebe25d800f484c0072
vasilkadt <96419451+vasilkadt@users.noreply.github.com>
2022-10-08 20:34:14 +0300

    Added solutions

---
+++ Advanced C#/DefiningClasses/01.Car/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car()
+            {
+                Make = "BMW",
+                Model = "x3"
+            };
+
+            Console.WriteLine($"{car.Make}: {car.Model}");
+        }
+    }
+}
---
+++ Advanced C#/DefiningClasses/02.CarExtension/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car()
+            {
+                Make = "BMW",
+                Model = "x3",
+                Year=2002,
+                FuelConsumption=0.4,
+                FuelQuantity=100
+            };
+
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
